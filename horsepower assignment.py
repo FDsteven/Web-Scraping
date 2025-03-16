b2aa707bf028e4802782f70824720ade93d0d1ad
Steven Shi <fdstevensdw@gmail.com>
2025-03-16 18:32:09 -0500

    Reformat csvs to matchup column names

--- horsepower assignment.py
+++ horsepower assignment.py
@@ -12,20 +12,29 @@ import glob
 # All_models = All_models.reset_index()
 # print(All_models)
 # All_models.to_csv("Locomotive HP Reference.csv")
-leasing_path = "Leasing Companies"
-HP_reference = pd.read_csv("Locomotive HP Reference.csv")
-file_name = "AEX.csv"
-raw_data = pd.read_csv(file_name)
-raw_data = raw_data[1:]
-df = raw_data.set_axis(['Road No.', 'Model', 'Build Date'], axis=1)
-df = df.reset_index()
-df['Horsepower'] = pd.DataFrame(np.zeros(shape=(len(df),1)), index=df.index)
-print(df)
+leasing_path = "Leasing Companies/*.csv"
+HP_reference = pd.read_csv("Locomotive HP Reference.csv",encoding='cp1252')
+# file_name = "AEX.csv"
+# raw_data = pd.read_csv(file_name)
+# raw_data = raw_data[1:]
+# df = raw_data.set_axis(['Road No.', 'Model', 'Build Date'], axis=1)
+# df = df.reset_index()
+# df['Horsepower'] = pd.DataFrame(np.zeros(shape=(len(df),1)), index=df.index)
+# print(df)
+Leasing_df = pd.DataFrame(np.zeros(shape=(0,6)),columns=['Company','Abbr','Road No.', 'Model', 'Date','HP'])
 for fname in glob.glob(leasing_path):
     print(fname)
-# for i in range(len(df)):
-#     if df.loc[i,"Model"] in All_models["Model"].values:
-#         print("True")
-#         row = All_models.loc[(All_models["Model"] == df.loc[i,"Model"])].index[0]
-#         df.loc[i,"Horsepower"] = All_models.loc[row,"Power Output"]
-print(df)
+    df_raw = pd.read_csv(fname)
+    df = df_raw[['Company','Abbr','Road No.', 'Model', 'Date']]
+    # df = df_raw.set_axis(['Company','Abbr','Road No.', 'Model', 'Build Date'], axis=1)
+    Leasing_df = pd.concat([Leasing_df,df],axis=0)
+    print(Leasing_df)
+Leasing_df = Leasing_df.reset_index()
+
+for i in range(len(Leasing_df)):
+    if Leasing_df.loc[i,"Model"] in HP_reference["Model"].values:
+        print(str(i) + "True")
+        row = HP_reference.loc[(HP_reference["Model"] == Leasing_df.loc[i,"Model"])].index[0]
+        Leasing_df.loc[i,"HP"] = HP_reference.loc[row,"Power Output"]
+print(Leasing_df)
+Leasing_df.to_csv("HP Assigned Leasing Company Master dataset.csv")
