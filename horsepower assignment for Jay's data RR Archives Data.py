71425a4b89b32cbfcd211f1c07c7fdf4fe9466fe
Steven Shi <fdstevensdw@gmail.com>
2025-05-07 14:25:42 -0500

    RR Archive Data scraped and consolidated, assigned HP to the exisitng HP reference

---
+++ horsepower assignment for Jay's data RR Archives Data.py
@@ -0,0 +1,42 @@
+import pandas as pd
+import numpy as np
+# manufacturers = ["GMD","EMD","GE"]
+# All_models = pd.DataFrame(np.zeros(shape=(0,4)),columns = ["Model","Build Year","Total Produced","Power Output"])
+# for manufacturer in manufacturers:
+#     model_list = manufacturer + " Locomotives.csv"
+#     models = pd.read_csv(model_list)
+#     models = models[["Model","Build Year","Total Produced","Power Output"]]
+#     All_models = pd.concat([All_models,models],axis=0)
+#     print(All_models)
+# All_models = All_models.reset_index()
+# print(All_models)
+# All_models.to_csv("Locomotive HP Reference.csv")
+RR_Archives_list = "RR Picture Archives List.xlsx"
+ArchiveList = pd.read_excel("RR Picture Archives List.xlsx")
+Consolidated = pd.DataFrame(np.zeros(shape=(0,4)),columns=['Unit Number', 'Notes', 'Model','Serial Number'])
+Companies = ArchiveList["Reporting Marks"].to_list()
+for company in Companies:
+    print(company)
+    filename = company + ".csv"
+    company_data = pd.read_csv(filename)
+    Consolidated = pd.concat([Consolidated,company_data],axis=0)
+Consolidated = Consolidated.reset_index()
+print(Consolidated)
+
+HP_reference = pd.read_csv("Locomotive HP Reference.csv",encoding='cp1252')
+# file_name = "AEX.csv"
+# raw_data = pd.read_csv(file_name)
+# raw_data = raw_data[1:]
+# df = raw_data.set_axis(['Road No.', 'Model', 'Build Date'], axis=1)
+# df = df.reset_index()
+# df['Horsepower'] = pd.DataFrame(np.zeros(shape=(len(df),1)), index=df.index)
+# print(df)
+for i in range(len(Consolidated)):
+    loco_model = Consolidated.loc[i,"Model"].rsplit(";",1)[0]
+    Consolidated.loc[i,"Model"] = loco_model
+    if Consolidated.loc[i,"Model"] in HP_reference["Model"].values:
+        print(str(i) + "True")
+        row = HP_reference.loc[(HP_reference["Model"] == Consolidated.loc[i,"Model"])].index[0]
+        Consolidated.loc[i,"HP"] = HP_reference.loc[row,"Power Output"]
+print(Consolidated)
+Consolidated.to_csv("HP Assigned RR Archive Data.csv")
--- scrape_from_RR_Pictures.py
+++ scrape_from_RR_Pictures.py
@@ -9,76 +9,94 @@ import time
 # %%
 # Base URL of the site
 # base_url = "https://www.thedieselshop.us/INDEXLSRS.html"
-base_url = "http://https://www.rrpicturearchives.net/locoList.aspx?id="
+base_url = "https://www.rrpicturearchives.net/locoList.aspx?id="
 
 # Function to get all links from the main page
 def get_all_links():
-    ArchiveList = pd.read_csv("RR Picture Archive List.csv")
+    ArchiveList = pd.read_excel("RR Picture Archives List.xlsx")
     links = []
-    Companies = ArchiveList["Reporting Marks  "].to_list()
+    Companies = ArchiveList["Reporting Marks"].to_list()
     for company in Companies:
         print(company)
-        links.append
+        links.append(company)
     
     return links
-get_all_links()
-# # Function to scrape table data from a given page
-# def scrape_table_data(url):
-#     response = requests.get(url)
-#     soup = BeautifulSoup(response.text, 'html.parser')
-#     tables = soup.find_all('table')
-    
-#     data = []
-#     for table in tables:
-#         rows = table.find_all('tr')
-#         for row in rows:
-#             cols = row.find_all(['td', 'th'])
-#             cols = [col.text.strip() for col in cols]
-#             if cols:  # Ensure it's not an empty row
-#                 data.append(cols)
-#     return data
+# Function to scrape table data from a given page
+def scrape_table_data(url):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    tables = soup.find_all('table')
+    data = []
+    for table in tables:
+        rows = table.find_all('tr')
+        for row in rows:
+            cols = row.find_all(['td', 'th'])
+            cols = [col.text.strip() for col in cols]
+            if cols:  # Ensure it's not an empty row
+                data.append(cols)
+    return data
 
-# # Main function to scrape all pages
-# def main():
-#     links = get_all_links(base_url)
-#     #! #################################################################################
-#     print(links)
-#     #return(links)
-#     #! #################################################################################
-#     all_data = []
-#     headers = None
+# Main function to scrape all pages
+def main():
+    links = get_all_links()
+    #! #################################################################################
+    print(links)
+    #return(links)
+    #! #################################################################################
+    all_data = []
+    headers = None
     
-#     for link in links:
-#         print(f"Scraping: {link}")
-#         table_data = scrape_table_data(link)
-#         table_data = pd.DataFrame(table_data[3:],index=None)
-#         link_name = link.rsplit("/",1)[1]
-#         link_name = link_name.rsplit(".",1)[0]
-#         csv_name = link_name + ".csv"
-#         table_data.to_csv(csv_name, index=False)
-#         print(table_data)
-#     #     if table_data:
-#     #         if headers is None:
-#     #             headers = table_data[0]  # Assume first row as header
-#     #         all_data.extend(table_data[1:])
+    for link in links:
+        link_url = base_url + link
+        print(f"Scraping: {link}")
+        table_data = scrape_table_data(link_url)
+        table_data = pd.DataFrame(table_data[20:])
+        table_data = table_data.iloc[:, [0,1,2,3]]
+        table_data.columns = ['Unit Number', 'Notes', 'Model','Serial Number']
+        cutoffrow = table_data[table_data['Unit Number'].str.startswith("Page")].index.min()
+        table_data = table_data.loc[0:cutoffrow-1]
+        for i in ["&Page=2","&Page=3"]:
+            try:
+                response = requests.head(link_url+i, allow_redirects=True, timeout=5)
+                if response.status_code == 200:
+                    print("URL exists!")
+                    additional_data = scrape_table_data(link_url+i)
+                    additional_data = pd.DataFrame(additional_data[20:])
+                    additional_data = additional_data.iloc[:, [0,1,2,3]]
+                    additional_data.columns = ['Unit Number', 'Notes', 'Model','Serial Number']
+                    cutoffrow = additional_data[additional_data['Unit Number'].str.startswith("Page")].index.min()
+                    additional_data = additional_data.loc[0:cutoffrow-1]
+                    table_data = pd.concat([table_data,additional_data],axis=0)
+                else:
+                    print(f"URL returned status code {response.status_code}")
+            except requests.RequestException as e:
+                print("URL does not exist or could not be reached:", e)
+        csv_name = link + ".csv"
+        table_data = table_data.replace({'Â ': '_'}, regex=True)
+        table_data.to_csv(csv_name, index=False)
+        print(table_data)
+    #     if table_data:
+    #         if headers is None:
+    #             headers = table_data[0]  # Assume first row as header
+    #         all_data.extend(table_data[1:])
         
-#     #     time.sleep(1)  # Respectful delay to avoid being blocked
+    #     time.sleep(1)  # Respectful delay to avoid being blocked
     
-#     # if all_data:
-#     #     df = pd.DataFrame(all_data, columns=headers)
-#     #     df.to_csv("diesel_data.csv", index=False)
-#     #     print("Data saved to diesel_data.csv")
-#     # else:
-#     #     print("No data scraped.")
+    # if all_data:
+    #     df = pd.DataFrame(all_data, columns=headers)
+    #     df.to_csv("diesel_data.csv", index=False)
+    #     print("Data saved to diesel_data.csv")
+    # else:
+    #     print("No data scraped.")
 
 
 
-# # %%
+# %%
 
-# links = main()
+links = main()
 
-# print(links)
-# # %%
+print(links)
+# %%
 
 
 # response = requests.get(base_url, verify= False)
