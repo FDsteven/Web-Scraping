a610e826f553f8b29ae22322d6bee8bd476cc36e
Steven Shi <fdstevensdw@gmail.com>
2025-05-28 15:50:28 -0500

    Adding private company to the RR dataframe

--- horsepower assignment for RR Archives Data.py
+++ horsepower assignment for RR Archives Data.py
@@ -13,16 +13,21 @@ import regex as re
 # print(All_models)
 # All_models.to_csv("Locomotive HP Reference.csv")
 def company_abbr_fetcher(string):
-    pattern = r"([A-Za-z]+)\s*\d"
+    pattern = r"^([^\s]+)"
     match = re.search(pattern, string)
-    num_pattern = r"\xa0(.+)"
+    num_pattern = r"\s+(.*)"
     reporting_number_match = re.search(num_pattern, string)
     full_name = ""
     if match:
         output = match.group(1)
         index_val = ArchiveList[ArchiveList['Reporting Marks'] == output].index
-        index_val = index_val[0]
-        company_name = ArchiveList.loc[index_val,"Railroad name"]
+        index_val_1 = Private_list[Private_list['Reporting Marks'] == output].index
+        if len(index_val) > 0:
+            val = index_val[0]
+            company_name = ArchiveList.loc[val,"Railroad name"]
+        if len(index_val_1) > 0:
+            val = index_val_1[0]
+            company_name = Private_list.loc[val,"Railroad name"]
         full_name = company_name
         reporting_mark = output
     else:
@@ -37,13 +42,18 @@ RR_Archives_list = "RR Picture Archives List.xlsx"
 ArchiveList = pd.read_excel("RR Picture Archives List.xlsx")
 Consolidated = pd.DataFrame(np.zeros(shape=(0,4)),columns=['Unit Number', 'Notes', 'Model','Serial Number'])
 Companies = ArchiveList["Reporting Marks"].to_list()
+Private_list = pd.read_excel("RR Private Owners.xlsx")
+Consolidated_Private = pd.DataFrame(np.zeros(shape=(0,4)),columns=['Unit Number', 'Notes', 'Model','Serial Number'])
+Companies = Private_list["Reporting Marks"].to_list()
+RR_Archive_df = pd.read_csv("HP Assigned RR Archive Data-sorted-TD.csv")
 for company in Companies:
     filename = "RR Pictures Archive/" + company + ".csv"
     company_data = pd.read_csv(filename)
-    Consolidated = pd.concat([Consolidated,company_data],axis=0)
-Consolidated = Consolidated.reset_index()
-print(Consolidated)
-
+    Consolidated_Private = pd.concat([Consolidated_Private,company_data],axis=0)
+    
+print(Consolidated_Private)
+Consolidated_Private = Consolidated_Private.reset_index()
+Consolidated_Private.to_csv("HP Assigned RR Private Data.csv")
 HP_reference = pd.read_csv("Locomotive HP Reference.csv",encoding='cp1252')
 # file_name = "AEX.csv"
 # raw_data = pd.read_csv(file_name)
@@ -52,25 +62,27 @@ HP_reference = pd.read_csv("Locomotive HP Reference.csv",encoding='cp1252')
 # df = df.reset_index()
 # df['Horsepower'] = pd.DataFrame(np.zeros(shape=(len(df),1)), index=df.index)
 # print(df)
-Company_name = np.zeros(len(Consolidated))
-Consolidated.insert(loc=0, column='Company Name', value=Company_name)
-Consolidated.insert(loc=1, column='Reporting Mark', value=Company_name)
-Consolidated.insert(loc=2, column='Reporting Number', value=Company_name)
-for i in range(len(Consolidated)):
-    loco_model = Consolidated.loc[i,"Model"].rsplit(";",1)[0]
-    Consolidated.loc[i,"Model"] = loco_model
-    if Consolidated.loc[i,"Model"] in HP_reference["Model"].values:
+Company_name = np.zeros(len(Consolidated_Private))
+Consolidated_Private.insert(loc=0, column='Company Name', value=Company_name)
+Consolidated_Private.insert(loc=1, column='Reporting Mark', value=Company_name)
+Consolidated_Private.insert(loc=2, column='Reporting Number', value=Company_name)
+for i in range(len(Consolidated_Private)):
+    loco_model = Consolidated_Private.loc[i,"Model"]
+    Consolidated_Private.loc[i,"Model"] = loco_model
+    if Consolidated_Private.loc[i,"Model"] in HP_reference["Model"].values:
         print(str(i) + "True")
-        row = HP_reference.loc[(HP_reference["Model"] == Consolidated.loc[i,"Model"])].index[0]
-        Consolidated.loc[i,"HP"] = HP_reference.loc[row,"Power Output"]
-print(Consolidated)
-Consolidated['Unit Number'] = Consolidated['Unit Number'].str.upper()
-Consolidated.to_csv("HP Assigned RR Archive Data.csv")
-Consolidated = pd.read_csv("HP Assigned RR Archive Data.csv")
-for i in range(len(Consolidated)):
-    print(Consolidated.loc[i,"Unit Number"])
-    full_name, reporting_mark, reporting_number = company_abbr_fetcher(Consolidated.loc[i,"Unit Number"])
-    Consolidated.loc[i,"Company Name"] = full_name
-    Consolidated.loc[i,"Reporting Mark"] = reporting_mark
-    Consolidated.loc[i,"Reporting Number"] = reporting_number
-Consolidated.to_csv("HP Assigned RR Archive Data.csv")
+        row = HP_reference.loc[(HP_reference["Model"] == Consolidated_Private.loc[i,"Model"])].index[0]
+        Consolidated_Private.loc[i,"HP"] = HP_reference.loc[row,"Power Output"]
+print(Consolidated_Private)
+Consolidated_Private['Unit Number'] = Consolidated_Private['Unit Number'].str.upper()
+Consolidated_Private.to_csv("HP Assigned RR Private Data.csv")
+Consolidated_Private = pd.read_csv("HP Assigned RR Private Data.csv")
+for i in range(len(Consolidated_Private)):
+    print(Consolidated_Private.loc[i,"Unit Number"])
+    full_name, reporting_mark, reporting_number = company_abbr_fetcher(Consolidated_Private.loc[i,"Unit Number"])
+    Consolidated_Private.loc[i,"Company Name"] = full_name
+    Consolidated_Private.loc[i,"Reporting Mark"] = reporting_mark
+    Consolidated_Private.loc[i,"Reporting Number"] = reporting_number
+Consolidated = pd.concat([RR_Archive_df,Consolidated_Private],axis=0)
+Consolidated = Consolidated.reset_index()
+Consolidated.to_csv("HP Assigned RR Archive Data_extended.csv")
---
+++ scrape_from_RR_Pictures_Private_Owners.py
@@ -0,0 +1,117 @@
+# %%
+
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+import time
+import os
+
+
+# %%
+# Base URL of the site
+# base_url = "https://www.thedieselshop.us/INDEXLSRS.html"
+base_url = "https://www.rrpicturearchives.net/locoList.aspx?id="
+
+# Function to get all links from the main page
+def get_all_links():
+    ArchiveList = pd.read_excel("RR Private Owners.xlsx")
+    links = []
+    Companies = ArchiveList["Reporting Marks"].to_list()
+    for company in Companies:
+        print(company)
+        links.append(company)
+    
+    return links
+# Function to scrape table data from a given page
+def scrape_table_data(url):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    tables = soup.find_all('table')
+    data = []
+    for table in tables:
+        rows = table.find_all('tr')
+        for row in rows:
+            cols = row.find_all(['td', 'th'])
+            cols = [col.text.strip() for col in cols]
+            if cols:  # Ensure it's not an empty row
+                data.append(cols)
+    return data
+
+# Main function to scrape all pages
+def main():
+    links = get_all_links()
+    #! #################################################################################
+    print(links)
+    #return(links)
+    #! #################################################################################
+    all_data = []
+    headers = None
+    
+    for link in links:
+        link_url = base_url + link
+        print(f"Scraping: {link}")
+        table_data = scrape_table_data(link_url)
+        table_data = pd.DataFrame(table_data[20:])
+        table_data = table_data.iloc[:, [0,1,2,3]]
+        table_data.columns = ['Unit Number', 'Notes', 'Model','Serial Number']
+        cutoffrow = table_data[table_data['Unit Number'].str.startswith("Page")].index.min()
+        table_data = table_data.loc[0:cutoffrow-1]
+        for i in ["&Page=2","&Page=3","&Page=4","&Page=5","&Page=6","&Page=7"]:
+            try:
+                response = requests.head(link_url+i, allow_redirects=True, timeout=5)
+                if response.status_code == 200:
+                    print("URL exists!")
+                    additional_data = scrape_table_data(link_url+i)
+                    additional_data = pd.DataFrame(additional_data[20:])
+                    additional_data = additional_data.iloc[:, [0,1,2,3]]
+                    additional_data.columns = ['Unit Number', 'Notes', 'Model','Serial Number']
+                    cutoffrow = additional_data[additional_data['Unit Number'].str.startswith("Page")].index.min()
+                    additional_data = additional_data.loc[0:cutoffrow-1]
+                    table_data = pd.concat([table_data,additional_data],axis=0)
+                else:
+                    print(f"URL returned status code {response.status_code}")
+            except requests.RequestException as e:
+                print("URL does not exist or could not be reached:", e)
+        csv_name = link + ".csv"
+        table_data = table_data.replace({'Â ': '_'}, regex=True)
+        folder_path = 'RR Pictures Archive/'
+        full_path = os.path.join(folder_path, csv_name)
+        table_data.to_csv(full_path, index=False)
+        print(table_data)
+    #     if table_data:
+    #         if headers is None:
+    #             headers = table_data[0]  # Assume first row as header
+    #         all_data.extend(table_data[1:])
+        
+    #     time.sleep(1)  # Respectful delay to avoid being blocked
+    
+    # if all_data:
+    #     df = pd.DataFrame(all_data, columns=headers)
+    #     df.to_csv("diesel_data.csv", index=False)
+    #     print("Data saved to diesel_data.csv")
+    # else:
+    #     print("No data scraped.")
+
+
+
+# %%
+
+links = main()
+
+print(links)
+# %%
+
+
+# response = requests.get(base_url, verify= False)
+# soup = BeautifulSoup(response.text, 'html.parser')
+
+
+
+# # %%
+
+
+
+# if __name__ == "__main__":
+#     main()
+
+
